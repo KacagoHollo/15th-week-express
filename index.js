c97e47d0ba88798c20aa2f13f2c7985851b4bcd6
IstvanPongracz <pongracz88@gmail.com>
2022-02-15 10:55:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 4000
+const fs = require('fs')
+
+app.use(cors())
+app.use(express.json())
+
+const users = require('./users.json')
+
+app.post('/api/signup', (req, res) => {
+    if (!req.body.name || !req.body.password) {
+        return res.status(400).json("Missing credentials")
+    } // return után megáll a folyamat!!!
+    const userExists = users.some(user => user.name === req.body.name)
+    if (userExists) {
+        return res.sendStatus(409)
+    }
+    const newUser = {
+        name: req.body.name,
+        password: req.body.password
+    }
+    users.push(newUser)
+
+    fs.writeFileSync('users.json', JSON.stringify(users, null, 4))
+    res.sendStatus(200)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
